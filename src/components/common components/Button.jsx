aee0c324a9e1da753bee60dfc1d58750e7193b54
Kasymov Marsel <marselkasymov23@gmail.com>
2022-02-22 20:20:14 +0600

    styled

--- src/components/common components/Button.jsx
+++ src/components/common components/Button.jsx
@@ -3,13 +3,13 @@ export default function CatalogButton(){
 const catalogButtonStyle = {
 width: '400px',
 height: '80px',
-curdor: 'pointer',
+cursor: 'pointer',
 fontSize: '30px',
 color:'white'
 
 }
 return (
-    
+
     <button style={catalogButtonStyle}>Посмотреть каталог</button>
 
 )
